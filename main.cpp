77a2ad8fc7217f95d5f8663aea3c2cf476b7d559
Liron Toledo <tldlir001@myuct.ac.za>
2018-08-01 12:17:35 +0200

    initial commit + a little code

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+
+    vector<pair> data = new vector();
+    data.push_back(pair(2,10));
+    data.push_back(pair(2,5));
+    data.push_back(pair(8,4));
+    data.push_back(pair(2,10));
+    data.push_back(pair(2,10));
+    data.push_back(pair(2,10));
+    data.push_back(pair(2,10));
+
+    
+
+    return 0;
+}
